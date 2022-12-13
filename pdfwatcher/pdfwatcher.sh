77962a44b4d52013b5db7c5661f7ec4290b8f215
Leon Connor Holm <leonconnorholm@gmail.com>
2022-12-13 20:38:22 +0100

    the keeping track of pdf files just to delete them later was kinda obsolete thanks to the tmp dir lol

--- pdfwatcher/pdfwatcher.sh
+++ pdfwatcher/pdfwatcher.sh
@@ -5,7 +5,7 @@ set -e
 
 # Define a directory where we will store the textfile that keeps track of
 # downloaded PDFs
-PDF_TRACKER_DIR="$HOME/.pdf-tracker"
+PDF_TRACKER_DIR="/tmp/pdf-tracker"
 
 # Create the directory if it doesn't exist
 mkdir -p "$PDF_TRACKER_DIR" || {
@@ -13,46 +13,20 @@ mkdir -p "$PDF_TRACKER_DIR" || {
     exit 1
 }
 
-# Define the path to the textfile
-PDF_TRACKER_FILE="$PDF_TRACKER_DIR/downloaded-pdfs.txt"
-
-touch -c "$PDF_TRACKER_FILE" || {
-    echo "Error: Failed to create File $PDF_TRACKER_FILE" >&2
-    exit 1
-}
-
-# Delete all PDFs listed in the textfile on system startup
-rm -f $(cat "$PDF_TRACKER_FILE" | tr '\n' ' ')
-clear_pdfs() {
-  # Read the file line by line
-  while IFS= read -r line
-  do
-    # Use the rm command to delete each line
-    rm -f "$line"
-  done < "$PDF_TRACKER_FILE"
-
-  # Clear the list of downloaded PDFs
-  echo -n > "$PDF_TRACKER_FILE"
-}
-
-
-clear_pdfs
-
 # Use inotifywait to monitor the directory where PDFs are downloaded
 # and open any new PDFs with Okular as they are downloaded
 inotifywait -m -e create --format '%f' "$HOME/Downloads" | while read pdf; do
     if [[ $pdf == *.pdf ]]; then
-	
 	# Wait for the file to be completely downloaded before opening it
 	# The maximum wait time is 1.3 seconds
-	while [[ ! -s "$HOME/Downloads/$pdf" ]] && [[ $i -lt 13 ]]; do
+	while [[ ! -s "$PDF_TRACKER_DIR/$pdf" ]] && [[ $i -lt 13 ]]; do
     	  sleep 0.1
     	  i=$(( i + 1 ))
 	done
+	# mv the downloaded file into the tmp dir
+	mv "$HOME/Downloads/$pdf" $PDF_TRACKER_DIR	
 
-        okular --unique "$HOME/Downloads/$pdf" &
-        # Add the PDF to the list of downloaded PDFs
-        echo "$HOME/Downloads/$pdf" >> "$PDF_TRACKER_FILE"
+        okular --unique "$PDF_TRACKER_DIR/$pdf" &
     fi
 done
 
