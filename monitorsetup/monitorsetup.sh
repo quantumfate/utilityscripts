36d57a18c688b8ebbc0e1b4605a1e6e3376eb117
Leon Connor Holm <leonconnorholm@gmail.com>
2022-12-26 16:55:52 +0100

    updated monitorsetup

--- monitorsetup/monitorsetup.sh
+++ monitorsetup/monitorsetup.sh
@@ -45,10 +45,10 @@ finalCommand=""
 if [ "$DOCKED" = true ] 
 then
 
-  # monitor # resolution # frequency # x relative from top # rotation # primary
-  left=("HDMI-A-0" "2560x1440" "60" "0" "normal" "no")
-  middle=("DisplayPort-1" "2560x1440" "60" "0" "normal" "no")
-  right=("eDP" "1920x1080" "60" "360" "normal" "yes")
+  # monitor # resolution # frequency # x relative from top # rotation # primary # dpi
+  left=("HDMI-A-0" "2560x1440" "60" "0" "normal" "no" "110")
+  middle=("DisplayPort-1" "2560x1440" "60" "0" "normal" "no" "110")
+  right=("eDP" "1920x1080" "60" "360" "normal" "yes" "120") # Laptop
 
   # Set the monitor variable for the current setup
   monitors=(left middle right)
@@ -57,7 +57,7 @@ else
 
   # monitor # resolution # frequency # x relative from top # rotation # primary
   # detailed tiling with --pos option
-  main=("eDP" "1920x1080" "60" "0" "normal" "yes" "yes")
+  main=("eDP" "1920x1080" "60" "0" "normal" "yes" "yes" "180")
 
   # Set the monitor variable for the current setup
   monitors=(main)
@@ -65,28 +65,7 @@ else
 fi
 
 current_position="0x0"
-max_screen_width=0
-appended_width=0
-
-## Get the sum of all connected screen widths
-for monitor in "${monitors[@]}"
-do
-  
-  # Get the values of the current montor
-  eval "values=(\"\${$monitor[@]}\")"
-
-  # Check if a monitor is actually connected
-  if xrandr --query | grep -q "${values[0]} connected"; then
-      
-      # Split the resolution of the current monitor into the width and height values
-      IFS='x' read -ra resolution_parts <<< "${values[1]}"
-      width=${resolution_parts[0]}
-      
-      max_screen_width=$(( max_screen_width + width ))
-      
-  fi
-done
-
+current_x_pos=0
 
 ## Build the xrandr command
 for monitor in "${monitors[@]}"
@@ -98,23 +77,22 @@ do
   # Check if a monitor is actually connected
   if xrandr --query | grep -q "${values[0]} connected"; then
 
+      # Set the dpi value for each individual screen
+      commandBuilder+=" --dpi ${values[6]}x${values[6]}"
+
       # Add the current monitor to the command
       commandBuilder+=" --output ${values[0]}"
 
       if [[ ${values[5]} == "yes" ]]; then
         commandBuilder+=" --primary"
       fi
-      
+     
       # Relative position on the y axis from the top
       y=${values[3]}
       
       # Split the resolution of the current monitor into the width and height values
       IFS='x' read -ra resolution_parts <<< "${values[1]}"
-      
       current_width=${resolution_parts[0]}
-      
-      # Determine the top left pixel for the current screen
-      current_x_pos=$(( ( max_screen_width + appended_width ) - max_screen_width ))
 
       # Update the current position to the right of the current monitor
       current_position="${current_x_pos}x$y"
@@ -124,7 +102,7 @@ do
 
       # adding the calculated position
       commandBuilder+=" --pos ${current_position}"
-      appended_width=$(( appended_width + current_width ))
+      current_x_pos=$(( current_x_pos + current_width ))
 
       finalCommand+="$commandBuilder"
       commandBuilder=""
